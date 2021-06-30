91adc2e6c37bfab366930d9e07d35e60c6e4db80
Raphaella Robles <raphaella.robles@gmail.com>
2021-06-30 00:02:13 +0000

    Added timer function that beeps after specified number of secs

---
+++ timer1.js
@@ -0,0 +1,22 @@
+// Implement an alarm clock / timer which will beep after a specified amount of time has passed. The user can specify an unlimited number of alarms using command line arguments
+
+// Returns array of command line arguments
+let input = (process.argv.slice(2));
+// Define function which beeps based on command line arguments
+const timer = function(input) {
+  // Iterate through every element of input
+  for (let i of input) {
+    // Check if input is a positive number
+    if (i > 0) {
+      // Beep wait time is converted from seconds to milliseconds
+      let seconds = i * 1000;
+      setTimeout( () => {
+        // Perform system sound after number of seconds
+        process.stdout.write('\x07');
+        console.log(`${i} seconds`);
+      }, seconds)
+    }
+  }
+};
+// Execute timer function
+timer(input);
